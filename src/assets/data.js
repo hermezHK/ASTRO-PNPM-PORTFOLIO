4cdb688b88d31a6119587f3aa1a9ca65d8ae73dc
Hermez Jaramillo <angelo_stk06@hotmail.com>
2023-09-27 22:49:02 -0500

    feat: add technologies style

--- src/assets/data.js
+++ src/assets/data.js
@@ -7,4 +7,43 @@ export const socials = [
 export const info = [
     { text: "About" },
     { text: "Projects"},
+];
+
+export const technologies = [
+    {
+        text: "TypeScript",
+        logo: "bx bxl-typescript"
+    },
+    {
+        text: "JavaScript",
+        logo: "bx bxl-javascript",
+    },
+    {
+        text: "PostgreSQL",
+        logo: "bx bxl-postgresql"
+    },
+    {
+        text: "AWS",
+        logo: "bx bxl-aws"
+    },
+    {
+        text: "Docker",
+        logo: "bx bxl-docker"
+    },
+    {
+        text: "CSS",
+        logo: "bx bxl-css3"
+    },
+    {
+        text: "Python",
+        logo: "bx bxl-python"
+    },
+    {
+        text: "React",
+        logo: "bx bxl-react"
+    },
+    {
+        text: "Flask",
+        logo: "bx bxl-flask"
+    },
 ]
